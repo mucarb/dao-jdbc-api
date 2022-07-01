84ad6d164701db213b8fd88f98ac3e6e5195f51e
mucarb <murilo.rb60@gmail.com>
2022-07-01 00:47:15 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,23 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Department obj = new Department(1, "Books");
-		
+
 		Seller seller = new Seller(21, "Andre", "andre@gmail.com", new Date(), 3000.00, obj);
-		
+
+		// O programa não conhece a implentação, apenas a interface
+		// forma de fazer injeção de dependência sem explicitar a implementação
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		System.out.println(seller);
 	}
 
--- src/model/dao/DapartmentDAO.java
+++ src/model/dao/DapartmentDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import model.entities.Department;
 
-public interface DapartmentDAO {
+public interface DapartmentDao {
 
 	void insert(Department obj);
 
