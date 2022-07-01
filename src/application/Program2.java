e26596ce3637393154e141c412891bb892df50ff
mucarb <murilo.rb60@gmail.com>
2022-07-01 16:04:00 -0300

    Department deleteById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,7 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
 		System.out.println("===== TEST 01: department findById =====");
@@ -19,17 +21,25 @@ public class Program2 {
 		System.out.println("\n===== TEST 02: department findAll =====");
 		List<Department> list = departmentDao.findAll();
 		list.forEach(System.out::println);
-		
+
 		System.out.println("\n===== TEST 03: department insert =====");
 		Department newDepartment = new Department(null, "Foods");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = " + newDepartment.getId());
-		
+
 		System.out.println("\n===== TEST 04: department update =====");
 		department = departmentDao.findById(6);
 		department.setName("Woodwork");
 		departmentDao.update(department);
 		System.out.println("Update complete");
+
+		System.out.println("\n===== TEST 05: department deleteById =====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed!");
+
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import db.DB;
 import db.DBException;
+import db.DBIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -70,8 +71,19 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("DELETE FROM department WHERE department.Id = ?");
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DBIntegrityException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
